10ab1a67afb7da4b0609d95d17703ae9e51a3adc
vmtuan12 <100104530+vmtuan12@users.noreply.github.com>
2024-06-27 00:13:55 +0700

    add functions in redis connector class

--- crawl_reviews/crawl_reviews/utils/redis_connector.py
+++ crawl_reviews/crawl_reviews/utils/redis_connector.py
@@ -1,7 +1,15 @@
 from redis import Redis
+from crawl_reviews.utils.tiki_utils import get_full_set_category_id, get_category_range
 
 class RedisConnector:
     _instance = None
+
+    SCRAPED_PAGES = "scraped-pages:"
+    CURRENT_SCRAPING_PAGES = "current-scraping-pages:"
+    READY_SCRAPE_PAGES = "ready-scrape-pages:"
+    CATEGORY_DONE = "category-done:"
+    CURRENT_PAGE_NUMBER_CATEGORY = "page-number:"
+
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(RedisConnector, cls).__new__(cls)
@@ -14,3 +22,22 @@ class RedisConnector:
     
     def close(self):
         self.redis_client.close()
+
+    def get_list_remaining_category_id_by_spider(self, spider_id: int) -> list:
+        done_category_set = self.redis_client.smembers(self.CATEGORY_DONE)
+
+        cate_range = get_category_range(spider_id=spider_id)
+        set_category = get_full_set_category_id()[cate_range[0]:cate_range[1]]
+
+        return list(set_category - done_category_set)
+    
+    def get_ready_scrape_page_by_spider(self, spider_id: int) -> int | None:
+        cate_range = get_category_range(spider_id=spider_id)
+        all_ready_scrape_page_key = self.redis_client.keys(pattern=f"{self.READY_SCRAPE_PAGES}*")
+
+        for key in all_ready_scrape_page_key:
+            cate_id = int(key.split(":")[0])
+            if cate_range[0] <= cate_id and cate_id < cate_range[1]:
+                return cate_id
+
+        return None
