57955af48f1e7301419e9fd20857a0ea91560280
vmtuan12 <100104530+vmtuan12@users.noreply.github.com>
2024-06-29 02:54:23 +0700

    version 1 of items

--- crawl_reviews/crawl_reviews/items.py
+++ crawl_reviews/crawl_reviews/items.py
@@ -7,16 +7,46 @@ import scrapy
 
 
 class Product(scrapy.Item):
-    pass
+    id = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
+    shop_id = scrapy.Field()
+    price = scrapy.Field()
+    spid = scrapy.Field()
+    sold = scrapy.Field()
+    brand = scrapy.Field()
 
 class Shop(scrapy.Item):
-    pass
+    id = scrapy.Field()
+    name = scrapy.Field()
+    follower = scrapy.Field()
+    url = scrapy.Field()
+    chat_response = scrapy.Field()
+    join_time = scrapy.Field()
 
 class User(scrapy.Item):
-    pass
+    id = scrapy.Field()
+    name = scrapy.Field()
+    total_review_written = scrapy.Field()
+    total_like_received = scrapy.Field()
+    join_time = scrapy.Field()
 
 class Review(scrapy.Item):
-    pass
+    id = scrapy.Field()
+    rating = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    created_at = scrapy.Field()
+    spid = scrapy.Field()
+    product_id = scrapy.Field()
+    owner_id = scrapy.Field()
+    like = scrapy.Field()
+    images = scrapy.Field()
 
 class ReviewChild(scrapy.Item):
-    pass
+    id = scrapy.Field()
+    parent_id = scrapy.Field()
+    owner_id = scrapy.Field()
+    content = scrapy.Field()
+    created_at = scrapy.Field()
+    commentator = scrapy.Field()
--- crawl_reviews/crawl_reviews/utils/item_builder.py
+++ crawl_reviews/crawl_reviews/utils/item_builder.py
@@ -1,7 +1,10 @@
 from crawl_reviews.items import Product, Shop, Review, User, ReviewChild
 from scrapy import Item
+from datetime import date, timedelta
 
-def build_item(source: dict, item_type: Item) -> Item:
+BASE_URL = "https://tiki.vn/"
+
+def generate_item(source: dict, item_type: Item) -> Item:
     if item_type == Product:
         return product(source)
     elif item_type == Shop:
@@ -12,23 +15,98 @@ def build_item(source: dict, item_type: Item) -> Item:
         return review_child(source)
     else:
         return user(source)
+    
+def _handle_exception_retrieving_value(data, *args):
+    if len(args) == 0:
+        return None
+    
+    try:
+        result = data.get(args[0])
+        for index in range(1, len(args)):
+            result = result.get(args[index])
+    
+    except Exception:
+        return None
+    
+    return result
 
 def product(source: dict) -> Product:
     result = Product()
-    pass
+    data = source["data"]
 
+    result["id"] = data["id"]
+    result["name"] = data["name"]
+    result["url"] = BASE_URL + data["url_path"]
+    result["price"] = data["original_price"]
+
+    if source.get("from_detail") == True:
+        result["shop_id"] = _handle_exception_retrieving_value(data, "current_seller", "id")
+        result["spid"] = _handle_exception_retrieving_value(data, "current_seller", "product_id")
+        result["sold"] = data["all_time_quantity_sold"]
+        result["brand"] = _handle_exception_retrieving_value(data, "brand", "name")
+
+    else:
+        result["shop_id"] = data["seller_id"]
+        result["spid"] = data["seller_product_id"]
+        result["sold"] = _handle_exception_retrieving_value(data, "quantity_sold", "value")
+        result["brand"] = _handle_exception_retrieving_value(data, "brand_name")
+
+    return result
+        
 def shop(source: dict) -> Shop:
     result = Shop()
-    pass
+
+    result["id"] = source["id"]
+    result["name"] = source["name"]
+    result["follower"] = source["total_follower"]
+    result["url"] = source["url"]
+
+    for info in source["info"]:
+        if info["type"] == "chat":
+            result["chat_response"] = info["title"]
+
+    result["join_time"] = str(date.today() - timedelta(days=source["days_since_joined"]))
+
+    return result
 
 def review(source: dict) -> Review:
     result = Review()
-    pass
+
+    result["id"] = source["id"]
+    result["rating"] = source["rating"]
+    result["title"] = source["title"]
+    result["content"] = source["content"]
+    result["created_at"] = source["created_at"]
+    result["spid"] = source["spid"]
+    result["product_id"] = source["product_id"]
+    result["owner_id"] = _handle_exception_retrieving_value(source, "customer_id")
+    result["like"] = source["thank_count"]
+
+    images = _handle_exception_retrieving_value(source, "images")
+    if (images != None):
+        result["images"] = [i["full_path"] for i in images]
+
+    return result
 
 def user(source: dict) -> User:
     result = User()
-    pass
+
+    result["id"] = source["id"]
+    result["name"] = source["name"]
+    result["total_review_written"] = _handle_exception_retrieving_value(source, "contribute_info", "summary", "total_review")
+    result["total_like_received"] = _handle_exception_retrieving_value(source, "contribute_info", "summary", "total_thank")
+    result["join_time"] = source["created_time"]
+
+    return result
 
 def review_child(source: dict) -> ReviewChild:
     result = ReviewChild()
-    pass
+
+    result["id"] = source["id"]
+    result["parent_id"] = source["review_id"]
+    result["owner_id"] = source["customer_id"]
+    result["content"] = source["content"]
+    result["created_at"] = source["create_at"]
+    result["commentator"] = source.get("commentator")
+
+    return result
