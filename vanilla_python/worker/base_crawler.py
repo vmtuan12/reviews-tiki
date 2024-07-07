f681d7dd0b6e01f45e3d568f7040c7d4d4da2728
vmtuan12 <100104530+vmtuan12@users.noreply.github.com>
2024-07-07 19:07:49 +0700

    rewrite the flow

--- vanilla_python/worker/base_crawler.py
+++ vanilla_python/worker/base_crawler.py
@@ -12,13 +12,18 @@ import json
 from urllib.parse import urlparse, parse_qs
 from utils.log_writer import write_log
 import traceback
+from threading import current_thread
+from threading import enumerate as threading_enumerate
 
 class BaseWorker:
  
     def __init__(self, id: int):
         self.id = id
+        self.max_thread = 6
+        self.pool = ThreadPoolExecutor(max_workers=self.max_thread)
+        self.working_threads = set()
+        self.futures_submitted = 0
 
-        self.pool = ThreadPoolExecutor(max_workers=5)
         self.scraped_pages = set()
         self.crawl_queue = Queue()
         self.redis_conn = RedisConnector()
@@ -31,48 +36,67 @@ class BaseWorker:
         self.topic_user = 'user'
         self.topic_shop = 'shop'
 
-        self.count_msg_buffer_kafka = 0
-
         self._start_requests()
 
     def run_web_crawler(self):
         while True:
             try:
+                if len(self.working_threads) == self.max_thread:
+                    time.sleep(0.25)
+                    continue
+
                 url_headers_type = self.crawl_queue.get(timeout=360)
                 url = url_headers_type[0]
+
                 if url not in self.scraped_pages:
-                    print("Scraping URL: {}".format(url))
+                    print("Submitting URL: {}".format(url))
                     self.scraped_pages.add(url)
 
                     job = self.pool.submit(self._scrape_page, url_headers_type)
-                    print(f"Submitted: {url}")
                     job.add_done_callback(self._post_scrape_callback)
                     time.sleep(0.5)
  
             except Empty:
                 self.producer.flush()
-                self.redis_conn.add_category_done()
                 return
             except Exception as e:
                 print(e)
                 return
 
     def _start_requests(self):
-        self.current_category_id = self.redis_conn.get_one_in_process_category_id(self.id)
-        if self.current_category_id == None:
-            remaining_cate = self.redis_conn.get_list_remaining_category_id_by_worker(worker_id=self.id)
+        remaining_cate = self.redis_conn.get_list_remaining_category_id_by_worker(worker_id=self.id)
+        if len(remaining_cate) > 0:
+            self.__request_product_category(remaining_cate=remaining_cate)
 
-            if len(remaining_cate) == 0:
-                return
-            
-            self.current_category_id = int(remaining_cate[0])
+        else:
+            remaining_shop = self.redis_conn.get_set_shop_from_shop_waiting_by_worker(worker_id=self.id)
+            if len(remaining_shop) > 0:
+                self.__request_shop(remaining_shop=remaining_shop)
 
-        url_headers_type = api_headers_list_item_by_category(category_id=self.current_category_id, url_key="", page=1)
+            else:
+                self.__request_reviews()
 
-        total_page = self._init_pages(url_headers_type=url_headers_type, type="CATEGORY", metadata={"cate_id": self.current_category_id})
+    def __request_product_category(self, remaining_cate: set):
+        for cate in remaining_cate:
+            cate_int = int(cate)
+            url_headers_type_init = api_headers_list_item_by_category(category_id=cate_int, url_key="", page=1)
+            total_page = self._init_pages(url_headers_type=url_headers_type_init, type="CATEGORY", metadata={"cate_id": cate_int})
 
-        for index in range(1, total_page + 1):
-            url_headers_type = api_headers_list_item_by_category(category_id=self.current_category_id, url_key="", page=index)
+            for index in range(1, total_page + 1):
+                url_headers_type = api_headers_list_item_by_category(category_id=cate_int, url_key="", page=index)
+                self.crawl_queue.put(url_headers_type)
+
+    def __request_shop(self, remaining_shop: set):
+        for shop in remaining_shop:
+            shop_id = int(shop)
+            url_headers_type = api_headers_shop_info(seller_id=shop_id)
+            self.crawl_queue.put(url_headers_type)
+
+    def __request_reviews(self)
+
+        for shop in remaining_shop:
+            shop_id = int(shop)
+            url_headers_type = api_headers_shop_info(seller_id=shop_id)
             self.crawl_queue.put(url_headers_type)
 
     def _init_pages(self, url_headers_type: tuple, type: str, metadata: dict) -> int | list:
@@ -87,7 +111,10 @@ class BaseWorker:
                 except Exception:
                     err = traceback.format_exc() + "\n" + url + "\n" + req.text
                     write_log(err=err, file_name="init_pages.log")
-                    raise Exception()
+
+                    time.sleep(4)
+
+                    return self._init_pages(url_headers_type, type, metadata)
 
                 if type == "CATEGORY" or type == "REVIEW":
                     pages = res.get("paging").get("last_page")
@@ -116,12 +143,16 @@ class BaseWorker:
         proxy = get_proxy()
         try:
             res = requests.get(url, headers=headers, proxies=proxy)
-            print(res)
+            # print(res)
             return (res, url, url_type)
         except requests.RequestException:
             return
  
     def _post_scrape_callback(self, res):
+        self.working_threads.add(current_thread().getName())
+        # print("\n Name of the current executing thread: ",
+        #     current_thread().getName(), '\n')
+        
         response, url, request_type = res.result()
         if response and response.status_code == 200:
             self._parse_data(response_data=json.loads(response.text), request_type=request_type, url=url)
@@ -129,6 +160,9 @@ class BaseWorker:
         else:
             time.sleep(2)
             self.scraped_pages.remove(url)
+
+        self.working_threads.remove(current_thread().getName())
+        # print("THREAD " + current_thread().getName() + " DONE")
 
     def _parse_data(self, response_data: dict, request_type: str, url: str):
         if request_type == PRODUCT:
@@ -151,32 +185,18 @@ class BaseWorker:
             product_id = parsed_item["id"]
             spid = parsed_item["spid"]
 
+            self.redis_conn.add_shop_waiting(shop_id=shop_id)
+
             if not self.redis_conn.product_has_been_produced(product_id=product_id, spid=spid):
                 self.produce_to_kafka(msg=parsed_item, topic=self.topic_product)
-
-            api_header_type = api_headers_shop_info(seller_id=shop_id)
-            self.crawl_queue.put(api_header_type)
-            self.redis_conn.add_scraped_product_wait_shop(shop_id=shop_id, product_id=product_id, spid=spid)
-
-            if self.redis_conn.product_in_scraped_product_wait_review(product_id=product_id, spid=spid):
-                pages = self.redis_conn.get_review_total_pages(product_id=product_id, spid=spid)
-
-            else:
-                api_header_type_init_review = api_headers_reviews(product_id=product_id, spid=spid, page=1)
-                last_page = self._init_pages(url_headers_type=api_header_type_init_review, type="REVIEW", metadata={ "product_id": product_id, "spid": spid })
-                pages = [x for x in range(1, last_page + 1)]
-
-                self.redis_conn.add_scraped_product_wait_review(product_id=product_id, spid=spid)
-
-            for p in pages:
-                api_headers_type_review = api_headers_reviews(product_id=product_id, spid=spid, page=int(p))
-                self.crawl_queue.put(api_headers_type_review)
+                self.redis_conn.add_scraped_product(product_id=product_id, spid=spid)
 
         url_parsed = urlparse(url)
         query_dict = parse_qs(url_parsed.query)
         
         if response_data.get("paging") != None:
-            self.redis_conn.remove_page_in_category_total_pages(cate_id=int(query_dict["category"][0]),
+            self.redis_conn.remove_page_in_category_total_pages(worker_id=self.id,
+                                                                cate_id=int(query_dict["category"][0]),
                                                                 page=int(query_dict["page"][0]))
         else:
             self.redis_conn.remove_cursor_in_shop_total_cursor(shop_id=int(query_dict["seller_id"][0]),
@@ -194,12 +214,9 @@ class BaseWorker:
 
         if self.redis_conn.shop_can_be_scraped(shop_id=shop_id):
             self.produce_to_kafka(msg=parsed_shop, topic=self.topic_shop)
-            self.redis_conn.remove_scraped_product_wait_shop(shop_id=parsed_shop["id"])
 
             url_header_type_init = api_headers_list_product_in_shop(shop_id=parsed_shop["id"], cursor=0)
             list_cursor = self._init_pages(url_headers_type=url_header_type_init, type="SHOP", metadata={ "shop_id": parsed_shop["id"] })
-
-            self.redis_conn.add_shop_wait_product(shop_id=parsed_shop["id"])
         
         else:
             list_cursor = self.redis_conn.get_shop_total_cursors(shop_id=shop_id)
@@ -214,7 +231,7 @@ class BaseWorker:
         for item in data:
             parsed_review = generate_item(source=item, item_type=REVIEW)
             self.produce_to_kafka(msg=parsed_review, topic=self.topic_review)
-            print(parsed_review)
+            # print(parsed_review)
 
             user = generate_item(source=item["created_by"], item_type=USER)
             self.produce_to_kafka(msg=user, topic=self.topic_user)
@@ -236,4 +253,5 @@ class BaseWorker:
         
         self.producer.send(topic, value=msg)
         self.producer.flush()
-        # print(msg)
+        print(topic, "---------------------------------------")
+        print(msg)
